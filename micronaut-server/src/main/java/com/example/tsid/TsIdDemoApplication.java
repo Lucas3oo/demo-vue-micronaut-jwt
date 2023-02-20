5581835f794cb22e6f0c51c9544106be571e69be
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-02-20 13:05:34 +0100

    init

---
+++ micronaut-server/src/main/java/com/example/tsid/TsIdDemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.tsid;
+
+import io.micronaut.runtime.Micronaut;
+
+public class TsIdDemoApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(TsIdDemoApplication.class, args);
+    }
+}
